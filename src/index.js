19e4c9cd54ef6cafead5987971ea25abf9372173
Liam Eller <liamthelastson@gmail.com>
2022-02-04 12:05:06 -0800

    WIP: add ui logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Galactic from "./js/galactic.js";
+
+$(document).ready(function() {
+  $("#submit").on("click", function() {
+    let age = $("#age").val();
+    let lifeExpectancy = $("#life-expect").val();
+    let planet = $("#planet").val();
+    let galactic = new Galactic(age, lifeExpectancy, planet);
+    let result = galactic.calculator();
+    
+  });
+});
