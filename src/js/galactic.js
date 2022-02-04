04e807b541d6d9fbed2475b98d3fac470c58d0e6
Liam Eller <liamthelastson@gmail.com>
2022-02-04 09:05:30 -0800

    pass test on my class constructor

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,6 +1,6 @@
 export default class Galactic {
   constructor(earthAge, planet) {
-    // this.earthAge = earthAge;
-    // this.planet = planet;
+    this.earthAge = earthAge;
+    this.planet = planet;
   }
 }
